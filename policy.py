d01f0fe14c93bdf71adcaff6972729c8853ccfd9
sarvesh211999 <sarvesh211999@gmail.com>
2021-07-03 16:27:35 +0530

    Running code for methane

--- policy.py
+++ policy.py
@@ -20,18 +20,27 @@ class PolicyNetwork(TorchModelV2, nn.Module):
         self.torch_sub_model = TorchFC(obs_space, action_space, num_outputs,
                                        model_config, name)
 
-        self.out = SlimFC(
-            in_size=3, out_size=self.num_outputs, activation_fn="linear")
-        self.values = SlimFC(in_size=3, out_size=1, activation_fn="linear")
+        self.layer_1 = SlimFC(
+            in_size=768, out_size=360, activation_fn="linear")
+        self.layer_2 = SlimFC(
+            in_size=360, out_size=180, activation_fn="relu")
+        self.layer_3 = SlimFC(
+            in_size=180, out_size=90, activation_fn="relu")
+        self.layer_4 = SlimFC(
+            in_size=90, out_size=self.num_outputs, activation_fn="linear")
+        self.values = SlimFC(in_size=90, out_size=1, activation_fn="linear")
 
         self._last_value = None
 
     
     def forward(self, input_dict, state, seq_lens):
         features = input_dict["obs"]
-        out = self.out(features)
-        self._last_value = self.values(features)
-        return out , []
+        out = self.layer_1(features)
+        out = self.layer_2(out)
+        out = self.layer_3(out)
+        final_out = self.layer_4(out)
+        self._last_value = self.values(out)
+        return final_out , []
 
     def value_function(self):
         return torch.squeeze(self._last_value, -1)
--- run.py
+++ run.py
@@ -24,7 +24,7 @@ ModelCatalog.register_custom_model("modelC", model_C)
 ModelCatalog.register_custom_model("modelH", model_H)
 
 act_space = spaces.Box(low=-0.5,high=0.5, shape=(3,))
-obs_space = spaces.Box(low=-10000,high=10000, shape=(3,))
+obs_space = spaces.Box(low=-10000,high=10000, shape=(768,))
 
 def gen_policy(atom):
     model = "model{}".format(atom)
@@ -40,7 +40,7 @@ def gen_policy(atom):
 policies = {"policy_C": gen_policy("C"),"policy_H": gen_policy("H")}
 policy_ids = list(policies.keys())
 
-def policy_mapping_fn(agent_id, **kwargs):
+def policy_mapping_fn(agent_id, episode, **kwargs):
     if agent_id.startswith("C"):
         pol_id = "policy_C"
     else:
@@ -48,34 +48,42 @@ def policy_mapping_fn(agent_id, **kwargs):
     return pol_id
 
 
-from ray.rllib.agents import ppo
 from ray.tune.registry import register_env
 from ray import tune
+import ray.rllib.agents.ppo as ppo
 import os
+from ray.tune.logger import pretty_print
 
 def env_creator(env_config):
     return ma_env.MA_env(env_config)  # return an env instance
 
 register_env("MA_env", env_creator)
 
-config={
-    "multiagent": {
+config = ppo.DEFAULT_CONFIG.copy()
+
+config["multiagent"] = {
         "policy_mapping_fn": policy_mapping_fn,
         "policies": policies,
         "policies_to_train": ["policy_C", "policy_H"],
-    },
-    "env":"MA_env",
-    "log_level":"DEBUG",
-    "framework": "torch",
-    "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")),
-    "env_config": {"atoms":["C", "H", "H", "H", "H"]}
-    
-}
+    }
 
-stop = {
-    "episode_reward_mean": 150.0,
-    "timesteps_total": 100,
-    "training_iteration": 20,
-}
-results = tune.run("PPO", stop=stop,config=config, verbose=1,checkpoint_at_end=True)
-ray.shutdown()
+config["log_level"] = "WARN"
+config["framework"] = "torch"
+config["num_gpus"] =  int(os.environ.get("RLLIB_NUM_GPUS", "0"))
+config["env_config"] =  {"atoms":["C", "H", "H", "H", "H"]}
+config["rollout_fragment_length"] = 10
+config["sgd_minibatch_size"] = 16
+config["train_batch_size"] = 160
+config["num_workers"] = 10
+
+print(pretty_print(config))
+
+# quit()
+ray.init()
+agent = ppo.PPOTrainer(config, env="MA_env")
+
+n_iter = 100
+for n in range(n_iter):
+    result = agent.train()
+    print(pretty_print(result))
+
